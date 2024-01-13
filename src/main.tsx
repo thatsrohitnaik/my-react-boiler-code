81662ac2caa2d6a29f66799379890a2ad14971f2
thatsrohitnaik <rohit24naik@gmail.com>
2024-01-13 12:20:29 +0530

    mirage js

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import  makeServer from "./mock/index.ts";
+
+const environment = process.env.NODE_ENV;
+
+if (environment !== "production") {
+  makeServer();
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <p>{environment}</p>
     <App />
   </React.StrictMode>,
 )
--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,7 +1,11 @@
-import { createServer } from "miragejs"
+import { createServer, Model } from "miragejs"
 
 export default function () {
     createServer({
+        models: {
+            reminder: Model,
+          },
+      
         routes() {
             this.get("/api/reminders", () => ({
                 reminders: [
@@ -10,6 +14,12 @@ export default function () {
                     { id: 3, text: "Work out" }
                 ],
             }))
+
+            this.post("/api/reminders", (schema:any, request:any) => {
+                let attrs = JSON.parse(request.requestBody)
+                console.log(schema, attrs)
+                return { reminder: attrs }
+            })
         },
     })
 }
